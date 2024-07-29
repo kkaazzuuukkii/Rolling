71f88ee6a63d3ccf0fe456f9d5c94b27b7d607f6
kkaazzuukkii <bogdankonoshko@gmail.com>
2024-07-29 16:09:52 +0300

    .

--- main.js
+++ main.js
@@ -3,37 +3,37 @@ const good = document.querySelector(".second_main_good")
 
 const array = [
     {
-        image: '/img/good2.jpg'
+        image: './img/good2.jpg'
     },
     {
-        image: '/img/good3.jpg'
+        image: './img/good3.jpg'
     },
     {
-        image: '/img/good4.jpg'
+        image: './img/good4.jpg'
     },
     {
-        image: '/img/good5.jpg'
+        image: './img/good5.jpg'
     },
     {
-        image: '/img/good6.jpg'
+        image: './img/good6.jpg'
     },
     {
-        image: '/img/good7.jpg'
+        image: './img/good7.jpg'
     },
     {
-        image: '/img/good8.jpg'
+        image: './img/good8.jpg'
     },
     {
-        image: '/img/good9.jpg'
+        image: './img/good9.jpg'
     },
     {
-        image: '/img/good10.jpg'
+        image: './img/good10.jpg'
     },
     {
-        image: '/img/good11.jpg'
+        image: './img/good11.jpg'
     },
     {
-        image: '/img/good12.jpg'
+        image: './img/good12.jpg'
     }
 ]
 
